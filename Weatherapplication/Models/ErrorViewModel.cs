4e52beb993a9ccbd7e279c979dd4ef91c9ae9196
Admin <Admin@DESKTOP-M9G0I31>
2026-05-27 15:22:55 +0530

    Add project files.

---
+++ Weatherapplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Weatherapplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
